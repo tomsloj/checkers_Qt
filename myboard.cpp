20228945be0275b7f61fbdd050655923b7ba1fd7
tomsloj <tomsloj@gmail.com>
2020-04-30 01:02:29 +0200

    add custom item

---
+++ myboard.cpp
@@ -0,0 +1,6 @@
+#include "myboard.h"
+
+MyBoard::MyBoard()
+{
+
+}
---
+++ myboard.h
@@ -0,0 +1,11 @@
+#ifndef MYBOARD_H
+#define MYBOARD_H
+
+
+class MyBoard : public QGraphicsItem
+{
+public:
+    MyBoard();
+};
+
+#endif // MYBOARD_H
